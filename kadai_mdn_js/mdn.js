6c726b53fc5233dd809a3cc1a1baac01aeffc99c
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-09-16 22:32:52 +0900

    kadai_mdnをアップ

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const today = new Date();
+
+console.log(today.getFullYear());
+console.log('年');
+console.log(today.getMonth());
+console.log('月');
+console.log(today.getDate());
+console.log('日');
